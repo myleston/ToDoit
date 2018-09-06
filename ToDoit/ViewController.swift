624071b3f326921cd04380a6ece0d5c03064585c
铭元 <DevilCat@mylestons-macbook-pro.local>
2018-09-06 14:16:45 +0900

    Initial Commit

---
+++ ToDoit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoit
+//
+//  Created by Myleston Law on 2018/09/06.
+//  Copyright © 2018 Myleston Law. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
